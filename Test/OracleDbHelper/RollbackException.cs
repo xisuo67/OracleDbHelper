4c31dd9913fab5eaf435aef75b28c6c42ef92ab7
xisuo67 <392706283@qq.com>
2018-09-07 21:21:17 +0800

    代码提交

---
+++ Test/OracleDbHelper/RollbackException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleDbHelper
+{
+    /// <summary>
+    /// 表示一个回滚事务的异常
+    /// </summary>
+    public sealed class RollbackException : System.Exception
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="message">异常信息</param>
+        public RollbackException(string message) : base(message)
+        {
+        }
+    }
+}
